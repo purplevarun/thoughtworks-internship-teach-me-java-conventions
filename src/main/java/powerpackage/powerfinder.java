4ed704c364375528473a0bb4c44fe03127bef775
purplevarun <purplevarun@gmail.com>
2022-01-11 13:01:02 +0530

    camelCase

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
 public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+    public static int OF(int base, int exponent) {
+        int originalBase = base;
+        while (exponent-- > 0){
+            base*=originalBase;
         }
-        return p;
+        return base;
     }
 }
